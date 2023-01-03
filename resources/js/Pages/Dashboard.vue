bb408971b1adc91c9f75bab1fe9ffdca431eac0f
NIYIBIZI HIRWA <61378957+hirwa1@users.noreply.github.com>
2023-01-03 22:22:14 +0200

    Clean Project, removing unwanted features

--- resources/js/Pages/Dashboard.vue
+++ resources/js/Pages/Dashboard.vue
@@ -1,6 +1,5 @@
 <script setup>
 import AppLayout from '@/Layouts/AppLayout.vue';
-import Welcome from '@/Components/Welcome.vue';
 </script>
 
 <template>
@@ -14,7 +13,9 @@ import Welcome from '@/Components/Welcome.vue';
         <div class="py-12">
             <div class="max-w-7xl mx-auto sm:px-6 lg:px-8">
                 <div class="bg-white overflow-hidden shadow-xl sm:rounded-lg">
-                    <Welcome />
+                    <p class="text-center p-5 font-bold text-lg">
+                        Welcome
+                    </p>
                 </div>
             </div>
         </div>
